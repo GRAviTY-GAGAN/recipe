3c399b1b58518f11a778d1d82d5073e2c0fd5a3f
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-12 20:10:26 +0530

    update

--- src/Ingredients.js
+++ src/Ingredients.js
@@ -11,14 +11,17 @@ const ingredients = () => {
                 <div className="list">
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">1 and 1/2 cups (150g) <strong>graham cracker crumbs</strong> (about 10 full sheet graham crackers)</label> <br />
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">5 Tablespoons (70g) <strong>unsalted butter</strong>, melted</label> <br/>
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">1/4 cup (50g) <strong>granulated sugar</strong></label> <br/>
                     </div>
                 </div>
@@ -27,30 +30,37 @@ const ingredients = () => {
                 <div className="list">
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">four 8-ounce blocks (904g) full-fat <strong>cream cheese</strong>, softened to room temperature</label> <br />
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">1 cup (200g) <strong>granulated sugar</strong></label> <br/>
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">1 cup (240g) full-fat <strong>sour cream</strong>, at room temperature</label> <br/>
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">1 teaspoon <strong>pure vanilla extract</strong></label> <br/>
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">2 teaspoons <strong>fresh lemon juice</strong> (optional, but recommended)</label> <br/>
                     </div>
                     <div className="item">
-                        <div className="box"/>
+                        {/* <div className="box"/> */}
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">3 large <strong>eggs</strong>, at room temperature</label> <br/>
                     </div>
                     <div className="item">
                         <div className="box"/>
+                        <input type='checkbox' className="box"/>
                         <label className="list-items">topping suggestions: <em style ={{fontWeight: "500"}}>salted caramel, lemon curd, strawberry topping, chocolate ganache, red wine chocolate ganache, fresh fruit, whipped cream, or raspberry sauce</em> (recipe in notes) </label> <br/>
                     </div>
                 </div>
